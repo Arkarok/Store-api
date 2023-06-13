884d031b3cf684e2db3f9867b72da5b703ed0461
Arkarok <47455232+Arkarok@users.noreply.github.com>
2023-06-13 16:59:27 -0500

    new method createProductos

---
+++ bin/Conexion/conexionBD.cs
@@ -0,0 +1,20 @@
+﻿namespace storeAPI.conexion
+{
+    public class conexionBD
+    {
+        private String conexion=string.Empty;
+
+        public conexionBD()
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).
+                AddJsonFile("appsettings.json").Build();
+
+            conexion = builder.GetSection("ConnectionStrings:conexion").Value;
+        }
+
+        public String conexionSQL()
+        {
+            return conexion;
+        }
+    }
+}
