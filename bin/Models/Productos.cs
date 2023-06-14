a214957c6bc873e83e767ea81c4e0a4b3b45108f
Arkarok <47455232+Arkarok@users.noreply.github.com>
2023-06-14 16:18:51 -0500

    Problems

--- bin/Models/Productos.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace storeAPI.Models
-{
-    public class Productos
-    {
-        public int id { get; set; }
-        public String? descripcion { get; set; }
-        public decimal precio { get; set; }
-    }
-}
